6d53acaa01d9be6ae4777400b989705f72aeee7f
SergioTapiaO <98850932+SergioTapiaO@users.noreply.github.com>
2022-03-14 21:07:02 +0100

    version 1 + way points
    version 1 + way points

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+var tamTablero = 10;
+var numeroWayPoint=1;
 var mode;
 var numIni=1;
 var numFin=1;
 var obstaculos = new Array(); //el array de obstaculos
+var wayPoint = new Array(); //el array de way points
 var ini = null; //la coordenada de inicio
 var fin = null; // la coordenada de fin
 
@@ -19,14 +22,12 @@ $(function () {
 function initTable() {
     let table = $("table");
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < tamTablero; i++) {
         let fila = $("<tr></tr>");
-        for (let j = 0; j < 5; j++) {
+        for (let j = 0; j < tamTablero; j++) {
             let col = $("<td></td>");
             col.addClass("f" + i);
             col.addClass("c" + j);
-            /*if (i === dFil && j === dCol)V
-                col.prop("id", "destination");*/
             col.on("click", function(){
                 // si selecciona una casilla sin indicar que tipo es
                 if(mode === undefined){
@@ -36,11 +37,6 @@ function initTable() {
                 }
             
             });
-            //col.on("click touchstart", clickPosition);
-            /*col.on("mouseover", function (event) {
-                if (mouseDown)
-                    clickPosition(event);
-            })*/
             fila.append(col);
         }
         table.append(fila);
@@ -54,7 +50,7 @@ function marcarCasillas(event){
     let numCol = eventoC.split('c')[1];
     let casilla = $('.'+eventoF+'.'+eventoC);
     //marca la casilla inicio
-    if(casilla.attr('class').search("inicio") == -1 && casilla.attr('class').search("fin") == -1 && casilla.attr('class').search("obstaculo") == -1){
+    if(casilla.attr('class').search("inicio") == -1 && casilla.attr('class').search("fin") == -1 && casilla.attr('class').search("obstaculo") == -1 && casilla.attr('class').search("wayPoint") == -1){
         switch(mode){
             case "INIT": 
                 if(numIni <= 1){
@@ -78,6 +74,13 @@ function marcarCasillas(event){
                 let nuevoO = new Cordenada(numFila, numCol, 0, 0, 0); //los valores de fin y dist en los obstaculos dan igual
                 obstaculos.push(nuevoO);
             break;
+            case "WAY": 
+                casilla.toggleClass("wayPoint");
+                casilla.append(numeroWayPoint); 
+                numeroWayPoint++;
+                let nuevoW = new Cordenada(numFila, numCol, 0, 0, 0); //los valores de fin y dist en los way points dan igual
+                wayPoint.push(nuevoW);
+            break;
         };
     }else{
         alert("Esa casilla ya esta marcada")
@@ -90,6 +93,7 @@ function select_mode(){
         let ini = $("#marcar_ini").is(":checked");
         let fin = $("#marcar_fin").is(":checked");
         let obs = $("#marcar_obs").is(":checked");
+        let way = $("#marcar_way").is(":checked");
         if(ini === true){
             mode = "INIT"
         }
@@ -99,35 +103,37 @@ function select_mode(){
         else if(obs === true){
             mode = "OBSTACLE"
         }
-        
+        else if(way === true){
+            mode = "WAY"
+        }
     })
 }
 
 function initAlgorithm(){
     //para iniciar el algoritmo tiene que haber al menos ini y fin
     if(numIni > 1 && numFin > 1){
-        ini.setFin(fin.numFila, fin.numCol);
-        let solucion = new AEstrella(ini, fin, obstaculos, 5, 5);
-        solucion.iniciar();
+        let solucion = new AEstrella(ini, fin, obstaculos,wayPoint, tamTablero, tamTablero);
         solucion.algoritmo();
-        let sol = solucion.arraySolucion();
-        imprimirSolucion(sol);
-        
+       
+        imprimirSolucion(solucion.returnArraySoluciones());      
     }
     else{
         alert("Tienes que marcar una coordenada inicial y otra final");
     }
-    //console.log(obstaculos);
 }
 
-function imprimirSolucion(array){
-    for(let i = 0; i < array.length; i++){
-        let coor = array[i];
-        let fila = coor.getFila();
-        let col = coor.getCol();
-        let casilla = $('.f'+fila+'.c'+col);
-        casilla.toggleClass("camino");
+function imprimirSolucion(soluciones){
+    let tamSol = soluciones.length;
+    for(let i = 0; i < tamSol; i++){
+        let array = soluciones.shift();
+        for(let j = 0; j < array.length; j++){
+            let coor = array[j];
+            let fila = coor.getFila();
+            let col = coor.getCol();
+            let casilla = $('.f'+fila+'.c'+col);
+            casilla.addClass("camino");
 
+        }
     }
 }
 
@@ -135,7 +141,9 @@ function resetear(){
     $('table > tr').remove();
     numIni=1;
     numFin=1;
+    numeroWayPoint=1;
     obstaculos = new Array();
+    wayPoint = new Array();
     ini = null; 
     fin = null; 
     initTable();
